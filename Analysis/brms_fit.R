1b609221a6d6691cd220dedec58d95389a443227
Thomas <tegorman13@gmail.com>
2024-03-10 23:57:25 -0400

    update brms_fit.R

--- Analysis/brms_fit.R
+++ Analysis/brms_fit.R
@@ -23,39 +23,51 @@ e2 <- readRDS(here("data/e2_08-04-23.rds"))
 e3 <- readRDS(here("data/e3_08-04-23.rds")) 
 d <- rbind(e1,e2,e3)
 testAll <- d |> filter(expMode2 == "Test")
-testE1 <- e1 |> filter(expMode2 == "Test") 
-testE2 <- e2 |> filter(expMode2 == "Test") 
-testE3 <- e3 |> filter(expMode2 == "Test") 
+testE1 <- e1 |> filter(expMode2 == "Test")
+testE2 <- e2 |> filter(expMode2 == "Test")
+testE3 <- e3 |> filter(expMode2 == "Test")
 
-prior = c(prior(normal(30,200),lb=0,class= Intercept))
+# testAll <- d |> filter(expMode == "test-Nf")
+# testE1 <- e1 |> filter(expMode == "test-Nf") 
+# testE2 <- e2 |> filter(expMode == "test-Nf") 
+# testE3 <- e3 |> filter(expMode == "test-Nf") 
+
+
+#prior = c(prior(normal(30,200),lb=0,class= Intercept))
 
 modelName <- "combo_testVxBand_RF_5K.rds"
 param_str <- paste(unlist(final_values), sep="_",collapse = "_")
 
-iv2 <- "Bt"
-
+iv2 <- "Bt_testNf"
+iv2 <<- "Bt_test6_"
 
 e1_e2_form <-  "condit * bandInt + (1 + bandInt|id)"
 e3_form <- "condit * bandInt * bandOrder + (1 + bandInt|id)"
 combo_form <- "condit * bandInt * bandOrder * fb + (1 + bandInt|id)"
 
-e1_e2_form <-  "condit * bandType + (1 + bandInt|id)"
-e3_form <- "condit * bandType * bandOrder + (1 + bandInt|id)"
-combo_form <- "condit * bandType * bandOrder * fb + (1 + bandInt|id)"
+# e1_e2_form <-  "condit * bandType + (1 + bandInt|id)"
+# e3_form <- "condit * bandType * bandOrder + (1 + bandInt|id)"
+# combo_form <- "condit * bandType * bandOrder * fb + (1 + bandInt|id)"
+
+
+e1_e2_form <-  "condit * bandType + (1 + bandType|id)"
+e3_form <- "condit * bandType * bandOrder + (1 + bandType|id)"
+combo_form <- "condit * bandType * bandOrder * fb + (1 + bandType|id)"
+
 
 
 
 run_model <- function(data, dv, formula_str, modelName_suffix, final_values) {
   formula <- as.formula(paste(dv, "~", formula_str))
-  modelName <- paste0(modelName_suffix, "_", format(Sys.time(), "%M%OS"), ".rds")
+  modelName <- paste0(modelName_suffix, "_",iv2, format(Sys.time(), "%M%OS"), ".rds")
   file_path <- paste0(here::here("data/model_cache", modelName))
   brm(formula,
       data = data,
       file = file_path,
       iter = final_values$niter,
       chains = final_values$nchain,
-      silent = 0,
-      prior = prior,
+      silent = 1,
+      #prior = prior,
       control = list(adapt_delta = final_values$adapt_delta, max_treedepth = final_values$max_treedepth))
 }
 
@@ -120,3 +132,36 @@ if(model_flags[4] == 1) {
   run_combo_model("dist", combo_form, final_values)
 }
 
+
+
+e1_e2_form <-  "condit * bandInt + (1 + bandInt|id)"
+e3_form <- "condit * bandInt * bandOrder + (1 + bandInt|id)"
+combo_form <- "condit * bandInt * bandOrder * fb + (1 + bandInt|id)"
+
+iv2 <<- "Band_test6_"
+
+
+
+if(model_flags[1] == 1) {
+  print(paste("Running e1 model for vx and dist"))
+  run_e1_model("vx", e1_e2_form, final_values)
+  run_e1_model("dist", e1_e2_form, final_values)
+}
+
+if(model_flags[2] == 1) {
+  print(paste("Running e2 model for vx and dist"))
+  run_e2_model("vx", e1_e2_form, final_values)
+  run_e2_model("dist", e1_e2_form, final_values)
+}
+
+if(model_flags[3] == 1) {
+  print(paste("Running e3 model for vx and dist"))
+  run_e3_model("vx", e3_form, final_values)
+  run_e3_model("dist", e3_form, final_values)
+}
+
+if(model_flags[4] == 1) {
+  print(paste("Running combo model for vx and dist"))
+  run_combo_model("vx", combo_form, final_values)
+  run_combo_model("dist", combo_form, final_values)
+}
